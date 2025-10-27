4e9330e10201179c98f621ab2e3067d9618da944
Giuseppe Leone <giuseppeleone@p058173.nat-gm.FH-Koeln.DE>
2025-10-27 12:52:28 +0100

    started to code the shoppinglist page with includes the connection to the database

--- Object Folder/DB/Shoppinglist.cs
+++ Object Folder/DB/Shoppinglist.cs
@@ -1,12 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 
+namespace WG_Random_Task_assigner.Object_Folder.DB
+{
 public class Shoppinglist{
 
     [Key]
     public int id {get; set;}
 
-    public string? name {get; set;}
+    public string? name {get; set;}="";
 
     public int? quantity {get; set;} =1;
     
+}
 }
